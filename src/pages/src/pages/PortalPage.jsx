c0aedb0c6a3152086c558a5eb9bffc7ae05aa066
Charles351983 <charles351983@gmail.com>
2026-04-08 08:33:47 -0500

    Create PortalPage.jsx

---
+++ src/pages/src/pages/PortalPage.jsx
@@ -0,0 +1,9 @@
+export default function PortalPage() {
+  return (
+    <div style={{ background: "black", color: "white", minHeight: "100vh", padding: "24px" }}>
+      <h1>Portal</h1>
+      <p>This will be login + checkout</p>
+      <a href="/" style={{ color: "gold" }}>Back Home</a>
+    </div>
+  );
+}
