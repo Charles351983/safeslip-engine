1321880a5728aee90407260ee86712029ee20fe4
Charles351983 <charles351983@gmail.com>
2026-04-08 08:34:39 -0500

    Create MemberPage.jsx

---
+++ src/pages/src/pages/src/pages/MemberPage.jsx
@@ -0,0 +1,9 @@
+export default function MemberPage() {
+  return (
+    <div style={{ background: "black", color: "white", minHeight: "100vh", padding: "24px" }}>
+      <h1>Member Area</h1>
+      <p>Premium picks will show here</p>
+      <a href="/" style={{ color: "gold" }}>Back Home</a>
+    </div>
+  );
+}
